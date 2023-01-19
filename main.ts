4c39359fc4e5fcdd425f8908ddcdb460bd41ecc3
acoolguy12 <122555202+acoolguy12@users.noreply.github.com>
2023-01-19 12:20:32 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -45,12 +45,14 @@ input.onButtonPressed(Button.AB, function () {
     music.playTone(392, music.beat(BeatFraction.Double))
     music.playTone(294, music.beat(BeatFraction.Double))
     for (let index = 0; index < 2; index++) {
-        music.playTone(262, music.beat(BeatFraction.Double))
+        for (let index = 0; index < 2; index++) {
+            music.playTone(262, music.beat(BeatFraction.Double))
+        }
+        music.playTone(392, music.beat(BeatFraction.Double))
+        music.playTone(440, music.beat(BeatFraction.Double))
+        music.playTone(330, music.beat(BeatFraction.Double))
+        music.playTone(294, music.beat(BeatFraction.Double))
     }
-    music.playTone(392, music.beat(BeatFraction.Double))
-    music.playTone(440, music.beat(BeatFraction.Double))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(294, music.beat(BeatFraction.Double))
     for (let index = 0; index < 2; index++) {
         music.playTone(262, music.beat(BeatFraction.Double))
     }
