f73e965e9db57f7fd3db12ed5f85a627c8598bbc
acoolguy12 <122555202+acoolguy12@users.noreply.github.com>
2023-01-19 12:14:03 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -39,25 +39,25 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index < 3; index++) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        music.playTone(294, music.beat(BeatFraction.Whole))
-        music.playTone(330, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Double))
+        music.playTone(294, music.beat(BeatFraction.Double))
+        music.playTone(330, music.beat(BeatFraction.Double))
     }
     music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Double))
     for (let index = 0; index < 2; index++) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Double))
     }
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Double))
+    music.playTone(440, music.beat(BeatFraction.Double))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Double))
     for (let index = 0; index < 2; index++) {
-        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(262, music.beat(BeatFraction.Double))
     }
     for (let index = 0; index < 2; index++) {
-        music.playTone(392, music.beat(BeatFraction.Whole))
+        music.playTone(392, music.beat(BeatFraction.Double))
     }
 })
 input.onButtonPressed(Button.B, function () {
