2dd431a8b4c6cd6a4f5bf10cffb0734d0b5784e7
acoolguy12 <122555202+acoolguy12@users.noreply.github.com>
2023-01-19 12:12:10 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -37,4 +37,60 @@ input.onButtonPressed(Button.A, function () {
             `)
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 3; index++) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        music.playTone(294, music.beat(BeatFraction.Whole))
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    }
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    for (let index = 0; index < 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    for (let index = 0; index < 2; index++) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    for (let index = 0; index < 2; index++) {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . # . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+})
 music.playMelody("C D E F G A B C5 ", 200)
